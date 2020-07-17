e87e99de10a9ba5b7b7c138454c6d9b48647d492
Artem Kropotov <sportarty@mail.ru>
2020-07-17 13:36:12 +0300

    1. Класс Builder

---
+++ src/main/java/ru/kropotov/lesson_1/Person.java
@@ -0,0 +1,15 @@
+package ru.kropotov.lesson_1;
+
+/**
+ * Created by Artem Kropotov on 17.07.2020
+ */
+public class Person {
+    String firstName;
+    String lastName;
+    String middleName;
+    String country;
+    String address;
+    String phone;
+    int age;
+    String gender;
+}
