c903e8ae257063ffc255fe65af4bddceefa27766
Artem Kropotov <sportarty@mail.ru>
2020-07-27 15:53:23 +0300

    3. Counter

--- src/main/java/ru/kropotov/lesson_3/Main.java
+++ src/main/java/ru/kropotov/lesson_3/Main.java
@@ -5,11 +5,11 @@ package ru.kropotov.lesson_3;
  */
 public class Main {
     public static void main(String[] args) {
-        new Main().play(10);
-
+        play(10);
+        count(10000000);
     }
 
-    synchronized public void play(int count) {
+    public static void play(int count) {
         PingPong pingPong = new PingPong();
 
         Thread t1 = new Thread(() -> {
@@ -44,5 +44,34 @@ public class Main {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void count(int count) {
+
+        MyLock myLock = new MyLock();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < count; i++) {
+                myLock.add(1);
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < count; i++) {
+                myLock.subtract(1);
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(myLock.getCounter());
     }
 }
